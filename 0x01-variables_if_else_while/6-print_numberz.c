a82ca1485f4b21e860cecf185142948a2b072950
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-09-09 03:27:35 +0100

    new file task: 6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Program prints from 0 to 9
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 48;
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
