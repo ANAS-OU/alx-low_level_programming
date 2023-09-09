096481ed59e54f5b586230daef17480c7a0da173
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-09-09 03:19:28 +0100

    new file task: 3.alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Program that prints the alphabet in lowercase and uppercase
+ *
+ * Return: 0
+ **/
+
+int main(void)
+{
+	char n, x;
+
+	n = 'a';
+	x = 'A';
+	while (n <= 'z')
+	{
+		putchar(n);
+		n++;
+	}
+	while (x <= 'Z')
+	{
+		putchar(x);
+		x++;
+	}
+	putchar ('\n');
+	return (0);
+}
