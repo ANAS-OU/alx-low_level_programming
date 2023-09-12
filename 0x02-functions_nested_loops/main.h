9a69a4053f05ab986d4f829cd12e63bca1866537
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-09-12 21:07:55 +0100

    update

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,15 @@
+#include "_putchar.c"
+
 /**
  * The prototypes of all your functions
  */
 
-#ifndef MAIN_H
-#define MAIN_H
+/**
+ * #ifndef MAIN_H
+ * #define MAIN_H
+ *
+ *
+ * #endif
+ */
 
 int _putchar(char c);
-
-#endif
