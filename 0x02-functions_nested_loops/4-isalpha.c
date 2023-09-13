93d0b1ae4549a2161525e424371c4951ab2f410d
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-09-13 17:39:42 +0100

    new file for task: 4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+/**
+ * _isalpha - Function that checks a character if is a letter.
+ * @c: Function param.
+ *
+ * Return: 1 if the character is lowercase or uppercase.
+ * otherwise return 0.
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
