9c1c4c11821265483394db9fe9041ac4a858878e
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-09-13 19:17:08 +0100

    update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,3 @@
-#include<stdlib.h>
 #include "main.h"
 
 /**
@@ -11,7 +10,10 @@
 
 int print_last_digit(int n)
 {
-	int last_digit = abs(n) % 10;
+	if (n < 0)
+		n = -n;
+
+	int last_digit = n % 10;
 
 	_putchar(last_digit + '0');
 	return (last_digit);
