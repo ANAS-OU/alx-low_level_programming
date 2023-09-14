ce3a3d38ce1e08471a6bf01aa0e357944ba35b93
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-09-14 01:55:05 +0100

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,8 +9,12 @@
 
 void print_to_98(int n)
 {
-	for (; n < 98; n++)
-		printf("%d, ", n);
+	if (n > 98)
+		for (; n > 98; n--)
+			printf("%d, ", n);
+	else
+		for (; n < 98; n++)
+			printf("%d, ", n);
 
 	printf("%d\n", n);
 }
