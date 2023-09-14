0b7bb2cb4189a15288d0f1ec29f1451e81b1b4e6
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-09-15 00:25:37 +0100

    new file task: 2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - function that computes the multiplication of
+ * two integers.
+ * @a: first param.
+ * @b: second param.
+ * Return: multiplication result.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
