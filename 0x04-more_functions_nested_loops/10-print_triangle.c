ffe9b18c7a007170aeb8ada8d430cb0c154e65bf
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-09-15 03:59:41 +0100

    update

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,14 +17,10 @@ void print_triangle(int size)
 		for (a = 1; a <= size; a++)
 		{
 			for (b = a; b < size; b++)
-			{
 				_putchar(' ');
-			}
 
 			for (b = 1; b <= a; b++)
-			{
 				_putchar('#');
-			}
 
 			_putchar('\n');
 		}
