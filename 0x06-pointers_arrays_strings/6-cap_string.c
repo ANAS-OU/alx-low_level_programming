7693bd2b547f37afdddea1458b9663657843ee2c
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-09-21 12:54:51 +0100

    update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,7 +15,11 @@ char *cap_string(char *str)
 	}
 	while (str[i])
 	{
-		if (str[i] < '0' || (str[i] > '9' && str[i] < 'A') || str[i] > 'z')
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+				str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+				str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+				str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+				str[i] == '}')
 		{
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] -= 32;
