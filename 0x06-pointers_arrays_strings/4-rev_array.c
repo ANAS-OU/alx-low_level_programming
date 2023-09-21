57de92eb13f7243d449c93e1392fe9c9f2409a58
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-09-21 01:39:00 +0100

    update task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,10 +9,12 @@ void reverse_array(int *a, int n)
 {
 	int i, j;
 
-	for (i = 0; i < n - 1; i++ && n--)
+	n--;
+	for (i = 0; i < n; i++)
 	{
 		j = a[i];
-		a[i] = a[n - 1];
-		a[n - 1] = j;
+		a[i] = a[n];
+		a[n] = j;
+		n--;
 	}
 }
