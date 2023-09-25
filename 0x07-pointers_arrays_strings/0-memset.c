afa0631f269c398b43bdbf3fdc906f60f5ebc1d7
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-09-25 17:12:44 +0100

    adding: README, 0-memset.c, main.h

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+/* _memset - function that fills memory with constant byte.
+ * @s: pointer param 1.
+ * @b: constant byte.
+ * @n: size of *s.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}
