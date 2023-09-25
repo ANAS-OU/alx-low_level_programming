fc2480726da22517a86a0f3a72625aedfa7dd58f
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-09-26 00:52:33 +0100

    new file task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+/**
+ * _strchr - function that locates a character
+ * in a string.
+ * @s: pointer param 1.
+ * @c: byte to search.
+ * Return: pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (s);
+}
