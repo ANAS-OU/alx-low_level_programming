cbc4a226df8ed59080f7b0128ad659df1d501c9e
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-09-26 02:43:57 +0100

    new file task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+/**
+ * function that gets the length of
+ * a prefix substring.
+ * @s: pointer param 1.
+ * @accept: pointer param 2.
+ * Return: integer substring size.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	unsigned int n;
+
+	n = 0;
+	while (*accept)
+	{
+		for (i = 0; s[i] != ' '; i++)
+		{
+			if (*accept == s[i])
+				n++;
+		}
+		accept++;
+	}
+	return (n);
+}
