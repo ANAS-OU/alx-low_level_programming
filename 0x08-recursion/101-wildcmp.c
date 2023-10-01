8481e5fac5b058f14a41a29cd6c580f989d15f08
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-10-01 16:35:10 +0100

    task_8: new file + main.h's update

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,30 @@
+/**
+ * wildcmp - function that checks two strings
+ * if considered identical.
+ * @s1: first string (pointer).
+ * @s2: second string (pointer).
+ * Return: 1 in case of success
+ * Otherwise returns 0.
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	/**
+	 * Base case: If both strings are empty, they are considered identical
+	 */
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	/**
+	 * If s2 contains '*', we have two possibilities:
+	 * 1. '*' matches an empty string in s1, so we move to the next
+	 *    character in s1.
+	 * 2. '*' matches one or more characters in s1, so we move to the
+	 *    next character in s2.
+	 */
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	return (0);
+}
