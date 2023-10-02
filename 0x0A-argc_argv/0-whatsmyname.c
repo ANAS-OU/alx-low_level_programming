6e60af4b70a6ec2c95311003e7a14951483f0032
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-10-02 17:26:09 +0100

    task_0: update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,6 +3,8 @@
 /**
  * main - program that prints its
  * name to stdout.
+ * @argc: counter for program parameters (integer).
+ * @argv: a list of strings (pointer of pointers).
  * Return: always 0.
  */
 int main(int argc, char **argv)
