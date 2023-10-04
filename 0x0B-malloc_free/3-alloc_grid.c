1ad86a65b6554006eea75465475d77eef5ee9d23
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-10-04 16:22:49 +0100

    task_3: update

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,11 @@ int **alloc_grid(int width, int height)
 	int **arr;
 	int i;
 
-	arr = malloc(height * sizeof(int*));
+	if (width < 0 || height < 0)
+		return (NULL);
+	arr = malloc(height * sizeof(int *));
+	if (arr == NULL)
+		return (NULL);
 	for (i = 0; i < height; i++)
 		arr[i] = malloc(width * sizeof(int));
 
