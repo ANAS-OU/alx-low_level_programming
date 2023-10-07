279f9513939cedaa3e0e587c582eb0b458bdb8bd
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-10-07 20:34:59 +0100

    task_3: update

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,7 +14,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	size = max - min + 2;
+	size = max - min + 1;
 	p = malloc(size * sizeof(int));
 	if (p == NULL)
 		return (NULL);
