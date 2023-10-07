d21cfcba9b95e132e3bbd69275b6c0bd6a96c4c5
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-10-07 15:56:58 +0100

    task_0: task file + README.md + header

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function that allocates memory
+ * using malloc.
+ * @b: reseved block size.
+ * Return: pointer to the block.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+	p = malloc(b);
+	if (p != NULL)
+		return (p);
+	exit (98);
+}
