b2fe6d4110b427a390ac3040accb93dc2db2989c
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-10-10 18:52:25 +0100

    task_101: update

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the result of
+ * multiplication of two given numbers.
+ * @argc: argments count.
+ * @argv: argments array.
+ * Return: always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 2]);
+	return (0);
+}
