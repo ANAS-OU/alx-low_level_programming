9edaec727cd2a5b34c023ec89d45ffc339091d85
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-10-11 15:34:09 +0100

    task_5: new file

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - Function that frees dogs
+ * that were created by malloc func.
+ * @d: The dog wanna free.
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
