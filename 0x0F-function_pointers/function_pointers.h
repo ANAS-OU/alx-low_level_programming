04deadeb6edab3601013623b5f55c18aacdcd7d3
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-10-11 21:33:09 +0100

    task_0: update

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
 void print_name(char *name, void (*f)(char *));
 
-#endif /* MAIN_H */
+#endif /* FUNCTION_POINTERS_H */
