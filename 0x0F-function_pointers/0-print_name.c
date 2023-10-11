d648b4a769277aade830e98bf75935c8f130b6de
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-10-11 21:23:32 +0100

    task_0: task file + header + README.md

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+/**
+ * print_name - function that uses a pointer to an other
+ * function to print a name.
+ * @name: the name to print.
+ * @f: pointer to an other function.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
