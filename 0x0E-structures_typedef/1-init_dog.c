0e919ba169fc8387acace51502e42ce58d043497
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-10-11 01:34:01 +0100

    task_1: new file

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog - function that initialize a variable of type <dog>.
+ * @d: the object dog.
+ * @name: the dog's name.
+ * @age: the dog's age.
+ * @owner: the dog's owner.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
