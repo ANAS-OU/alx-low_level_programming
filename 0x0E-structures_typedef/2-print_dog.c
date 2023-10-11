e6dc7598bd0c1faec6174e0a96754bb653fa77cb
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-10-11 14:01:03 +0100

    task_2: update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
-		printf((d->age) ? "Age: %f", d->age : "Age: (nil)");
+		(d->age) ? printf("Age: %f\n", d->age) : printf("Age: (nil)\n");
 		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
 	}
 }
