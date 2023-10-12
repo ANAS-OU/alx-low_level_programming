3b2f6ea0cbf19e549eaeffce1958ab29f353f356
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-10-12 13:11:29 +0100

    task_3: simple calculator | all files

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "3-calc.h"
+
+/**
+ * main - program that performs simple operations
+ * @argc: the count of argments.
+ * @argv: the array of agrments.
+ *
+ * Return: result of operatoin followed by a new line.
+ */
+int main(int argc, char *argv[])
+{
+	int rst;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (atoi(argv[3]) == 0 && (strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	operation = get_op_func(argv[2]);
+	if (!operation)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	rst = operation(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", rst);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - function that returns the addition
+ * of two integers.
+ * @a: first param.
+ * @b: second param.
+ * Return: result of the addition.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function that returns the subtraction
+ * of two integers.
+ * @a: first param.
+ * @b: second param.
+ * Return: result.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function that returns the multiplication
+ * of two integers.
+ * @a: first param.
+ * @b: second param.
+ * Return: result.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function that returns the division
+ * of two integers.
+ * @a: first param.
+ * @b: second param.
+ * Return: result.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function that returns the modulo
+ * of two integers.
+ * @a: first param.
+ * @b: second param.
+ * Return: result.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
