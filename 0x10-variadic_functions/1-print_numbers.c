7dea0802ecb2896237d4240a5f3e5fec323b6c47
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-10-12 21:43:08 +0100

    task_1: update

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,6 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			printf("%s", separator);
 	}
 
-	printf("%d\n", va_arg(args, int));
+	printf("%d", va_arg(args, int));
+	printf("\n");
 	va_end(args);
 }
