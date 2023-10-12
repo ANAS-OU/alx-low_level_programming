3a487c6652ea91cef02dfe2e206c4dc8c2ed79eb
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-10-12 21:00:42 +0100

    task_1: new file + header update

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function that prints numbers,
+ * followed by a new line.
+ * @separator: separating string.
+ * @n: numbers count to print.
+ *
+ * Return: nothing.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (separator)
+			printf("%s", separator);
+	}
+
+	printf("%d\n", va_arg(args, int));
+	va_end(args);
+}
