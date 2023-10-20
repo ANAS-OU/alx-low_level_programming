d9f82ff30ee5e5dcdfce8382ef497dbcb3daac77
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-10-20 02:49:13 +0100

    some fixing for betty style

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,13 +2,13 @@
 void me_first(void) __attribute__((constructor));
 
 /**
- * me_first - function that prints to the stdout
- * a string before the main function is executed.
+ * me_first - function that prints to the stdout a string
+ * before the main function is executed.
  *
  * Return: nothing.
  */
 void me_first(void)
 {
-	printf("You're beat! and yet, you must allow,");
-	printf("\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
