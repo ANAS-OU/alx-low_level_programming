6d9e4d6e13745589f696bba6192a2400333f2698
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-10-23 17:50:08 +0100

    creating an other function that frees elements of a given list, and intialize head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a given list.
+ * @head: pointer to the list head.
+ *
+ * Return: nothing.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+}
