4a453f4d81822fd31e50c9da30216261f04695ad
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-10-29 02:16:08 +0100

    Creating a function checks endianess for owr machine

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * get_endianness - function that checks the endianness
+ * the use owr hardware.
+ *
+ * Return: 0 if big endian, 1 if little endian.
+ */
+int get_endianness(void)
+{
+	unsigned int n = 1; /* 0x01 */
+	int first_byte = (int) ((char *)&n)[0];
+	
+	/*
+	 * first_byte:
+	 * 0 - big endian.
+	 * 1 - little endian.
+	 */
+	return (first_byte);
+}
