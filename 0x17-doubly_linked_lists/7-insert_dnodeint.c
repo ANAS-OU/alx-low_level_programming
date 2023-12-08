3dc819cf3dcd45e9cb661de705aacc929806128e
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-12-08 17:40:10 +0100

    7. Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -28,6 +28,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new_node->next = *h;
+		if (*h)
+			(*h)->prev = new_node;
 		*h = new_node;
 	}
 	else
