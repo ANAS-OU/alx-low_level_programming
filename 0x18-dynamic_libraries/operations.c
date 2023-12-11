c3277e9b0a11b1bc943bc391a3017b393b9c14f3
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-12-11 22:20:04 +0100

    2. Let's call C functions from Python

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,59 @@
+/**
+ * add - function that returns the addition
+ * of two integers.
+ * @a: first param.
+ * @b: second param.
+ * Return: result of the addition.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - function that returns the subtraction
+ * of two integers.
+ * @a: first param.
+ * @b: second param.
+ * Return: result.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - function that returns the multiplication
+ * of two integers.
+ * @a: first param.
+ * @b: second param.
+ * Return: result.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - function that returns the division
+ * of two integers.
+ * @a: first param.
+ * @b: second param.
+ * Return: result.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - function that returns the modulo
+ * of two integers.
+ * @a: first param.
+ * @b: second param.
+ * Return: result.
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
