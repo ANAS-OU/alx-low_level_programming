120145be56bb7735d8716c37ae41714c3e8d1ee5
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-12-23 00:35:28 +0100

    4. >>> ht['betty']

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,6 +14,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	long unsigned int idx;
 	hash_node_t *head;
 
+	if (!ht || !key)
+		return (NULL);
+
 	idx = key_index((const unsigned char *)key, ht->size);
 	head = ht->array[idx];
 
