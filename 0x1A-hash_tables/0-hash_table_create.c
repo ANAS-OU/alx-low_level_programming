958b51c28b3235d439208567749db585b695732e
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-12-22 23:49:44 +0100

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,6 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_ht;
-	long unsigned int i;
 
 	new_ht = malloc(sizeof(hash_table_t));
 	if (!new_ht)
@@ -25,9 +24,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(new_ht);
 		return (NULL);
 	}
-
-	for (i = 0; i < size; i++)
-		new_ht->array[i] = NULL;
 
 	return (new_ht);
 }
