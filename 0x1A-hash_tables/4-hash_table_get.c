474ec4e746fb55debdfd9bb68ee674b517c39a61
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-12-23 00:33:20 +0100

    4. >>> ht['betty']

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - a function that retrieves
+ * a value associated with a key.
+ * @ht: our hash table
+ * @key: looking node key
+ *
+ * Return: the value associated with the element,
+ * or NULL if key couldn’t be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	long unsigned int idx;
+	hash_node_t *head;
+
+	idx = key_index((const unsigned char *)key, ht->size);
+	head = ht->array[idx];
+
+	while (head)
+	{
+		if (!strcmp(head->key, key))
+			return (head->value);
+		head = head->next;
+	}
+
+	return (NULL);
+}
